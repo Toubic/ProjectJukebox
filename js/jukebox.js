02e7eceff1f4f0489a7a30c6937f7c5efcab2e74
Tobias Johansson <tj22fe@student.lnu.se>
2016-05-13 03:19:30 +0200

    small correction

--- js/jukebox.js
+++ js/jukebox.js
@@ -5,6 +5,8 @@ var theJukebox = document.importNode(theTemplate.content.firstElementChild, true
 var theBody = document.body;
 theBody.appendChild(theJukebox);
 
+var buttons = theJukebox.querySelectorAll("input");
+
 for (var i = 0; i < buttons.length; i++) {
     theJukebox.querySelectorAll("input")[i].addEventListener("click", function (event) {
         var playASong = document.querySelector("#playASong");
