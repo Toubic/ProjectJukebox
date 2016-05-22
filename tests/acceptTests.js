cad4bd5de108e73c4d19ec19bc09ba1adc1f120f
Tobias Johansson <tj22fe@student.lnu.se>
2016-05-22 03:03:01 +0200

    + new acceptance test

--- tests/acceptTests.js
+++ tests/acceptTests.js
@@ -60,3 +60,51 @@ module.exports = {
             .end();
     }
 };
+
+module.exports = {
+    "Test ability to create a jukebox and import another users jukebox" : function (client) {
+        client
+            .url("https://jukeboxvids.herokuapp.com/")
+            .waitForElementVisible("body", 1000)
+            .assert.title("Project Jukebox")
+            .setValue("input[type=text]", "user1")
+            .setValue("input[type=password]", "password1")
+            .click("input[type=submit]")
+            .pause(1000)
+            .waitForElementPresent("a", 1000)
+            .assert.containsText("a", "Create a new jukebox")
+            .click("a")
+            .waitForElementVisible("textarea", 1000)
+            .setValue("input[type=text]", "jukebox1")
+            .setValue("textarea", "https://www.youtube.com/watch?v=yE8mK2Jkjys")
+            .click("input[type=submit]")
+            .pause(1000)
+            .assert.containsText("h1", "jukebox1")
+            .url("https://jukeboxvids.herokuapp.com/login")
+            .waitForElementVisible("body", 1000)
+            .assert.title("Project Jukebox")
+            .setValue("input[type=text]", "user2")
+            .setValue("input[type=password]", "password2")
+            .click("input[type=submit]")
+            .pause(1000)
+            .waitForElementPresent("a", 1000)
+            .assert.containsText("a", "Create a new jukebox")
+            .click("a")
+            .waitForElementVisible("textarea", 1000)
+            .setValue("input[type=text]", "jukebox2")
+            .setValue("textarea", "https://www.youtube.com/watch?v=s8eGujRpXyU")
+            .click("input[type=submit]")
+            .pause(1000)
+            .assert.containsText("h1", "jukebox2")
+            .click("a:nth-of-type(2)")
+            .waitForElementVisible("body", 1000)
+            .assert.containsText("h1", "Search for a jukebox")
+            .setValue("input[type=text]", "1")
+            .click("input[type=submit]")
+            .pause(1000)
+            .assert.containsText("h1", "jukebox1")
+            .click("input")
+            .waitForElementPresent("iframe", 1000)
+            .end();
+    }
+};
